b663e9c76a14f65491924548903eb1bf6d536fe2
Gage <gageeide@gmail.com>
2026-03-07 16:42:37 -0600

    Set server to use secure connection whenever not in development mode

--- server.js
+++ server.js
@@ -80,7 +80,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   },
 }));
